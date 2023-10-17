afba9b73c3548abbec4d6b7ccdc44b7ba17027ee
Carl Leitner <litlfred@gmail.com>
2023-10-17 15:35:47 -0400

    Update WhoMapper.kt

--- verify/src/main/java/org/who/ddccverifier/verify/hcert/who/WhoMapper.kt
+++ verify/src/main/java/org/who/ddccverifier/verify/hcert/who/WhoMapper.kt
@@ -2,7 +2,7 @@ package org.who.ddccverifier.verify.hcert.who
 
 import org.hl7.fhir.r4.model.Bundle
 import org.who.ddccverifier.verify.BaseMapper
-import org.who.ddccverifier.verify.hcert.dcc.logical.WHOLogicalModel
+import org.who.ddccverifier.verify.hcert.dcc.logical.WHO_CoreDataSet
 
 /**
  * Translates a DDCC QR CBOR object into FHIR Objects
